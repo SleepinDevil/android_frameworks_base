8aa9508b6b17169669a51f0bde60ece5fff1b742
romanbb <romanbirg@gmail.com>
2012-07-12 14:36:11 -0500

    SystemUI: Rewrite toggles
    AOKP - Like toggles, more AOSP looking, cleaned up a bit the original commit and
    fixed consistency with CM
    
    Patchset 2: remove fast charge drawables
    Patchset 3: Implement 2G toggle interface
    Patchset 4 & 5: Whitespaces, tabs, indenting
    Patchset 6:
    	* Moar fixes, tablet
    	* Rework 2G toggle
    Patchset 7: Sweetspot for three modes, margin fixes
    Patchset 8:
    	* Relative layout and ellipsizing of text
            * Small margin adjustment
    Patchset 9: Remove unused layout, restore match_parent
    Patchset 10: TabUI margins, goodbye tabUI brightness-controller
    Patchset 11: typo
    Patchset 12: Consistency with tethering options
    Patchset 13: Remove unused drawables
    Patchset 14: Phone & PhabUI margin adjustments
    Patchset 16 & 17: es_ES
    Patchset 18: Fix brightness behaviour. Ready to merge
    
    Change-Id: Idaea739b6781cfa784991e46ad0675471c9f92a7
    
    Conflicts:
    
    	packages/SystemUI/res/layout/status_bar_expanded.xml
    	packages/SystemUI/res/layout/system_bar_settings_view.xml
    	packages/SystemUI/res/values-es/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerWidget.java
    	packages/SystemUI/src/com/android/systemui/statusbar/tablet/SettingsView.java
    
    http://review.paranoid-rom.com/#/c/58/

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -976,4 +976,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
         return km.inKeyguardRestrictedInputMode();
     }
+
+    public boolean isTablet() {
+        return false;
+    }
 }
